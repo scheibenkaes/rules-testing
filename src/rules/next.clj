02b73a1d975be84e5287e5ed88211b6a2326ea8a
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2016-12-15 17:25:30 +0100

    add simple acc example

--- src/rules/next.clj
+++ src/rules/next.clj
@@ -1,6 +1,8 @@
 (ns rules.next
-  (:require [clara.rules :refer :all])
-  (:require [rules.core :refer [patients]])
+  (:require [clara.rules :refer :all]
+            [clara.rules.accumulators :as acc]
+            [rules.core :refer [patients]]
+            [clj-time.core :as joda])
   (:import [rules.core Patient]))
 
 (defrecord Tall [id])
@@ -26,19 +28,62 @@
   ;; do stuff if rule matches
   (println "Promo for " ?id))
 
+(defrecord Purchase [id date price])
+
+(defrecord Total [total])
+
+(def purchases [(->Purchase "Iggy" (joda/date-time 2015 4 1) 33.3)
+                (->Purchase "Madonna" (joda/date-time 2015 6 3) 120.0)
+                (->Purchase "Iggy" (joda/date-time 2015 4 1) 166.0)
+                (->Purchase "Edwin" (joda/date-time 2016 1 1) 34)
+                (->Purchase "Franz" (joda/date-time 2015 3 27) 89)])
+
+(defrule total-purchases
+  [?total <- (acc/sum :price) :from [Purchase]]
+  =>
+  (insert! (->Total ?total)))
+
+(defquery purchases-in-year [:?year]
+  [?purchase <- Purchase
+   (= ?id id)
+   (= ?date date)
+   (= ?year (joda/year date))
+   (joda/within?
+    (joda/date-time ?year 1 1)
+    (joda/date-midnight (inc ?year) 1 1)
+    date)])
+
+(defquery total []
+  [?total <- Total])
+
 (def session2 (mk-session 'rules.next))
 
 (comment
 
- (-> session2
-     (insert-all patients)
-     (fire-rules)
-     (query tall-people)
-     )
- )
+  (-> session2
+      (insert-all patients)
+      (fire-rules)
+      (query tall-people)
+      )
 
-;;; what's it all about?
+  (-> session2
+      (insert-all purchases)
+      (fire-rules)
+      (query total)
+      )
+
+    (-> session2
+      (insert-all purchases)
+      (fire-rules)
+      (query purchases-in-year :?year 2016)
+      )
+  
+  )
+
+;;; what to do with this?
 ;;
+;; Medical recommendations
+;; If-else customer logic
 
 ;; use it with Java http://www.clara-rules.org/docs/java/
 ;;
